3ab2f2819642d210bdc8d81bcb4c7b47ac0086ff
Christian Wach <needle@haystack.co.uk>
2022-06-21 16:11:32 +0100

    Bump version to 0.7.3a

--- civicrm-event-organiser.php
+++ civicrm-event-organiser.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CiviCRM Event Organiser
  * Description: Sync Event Organiser Events with CiviCRM Events.
- * Version: 0.7.2
+ * Version: 0.7.3a
  * Author: Christian Wach
  * Author URI: https://haystack.co.uk
  * Plugin URI: https://github.com/christianwach/civicrm-event-organiser
@@ -16,7 +16,7 @@
 
 
 // Set our version here.
-define( 'CIVICRM_WP_EVENT_ORGANISER_VERSION', '0.7.2' );
+define( 'CIVICRM_WP_EVENT_ORGANISER_VERSION', '0.7.3a' );
 
 // Store reference to this file.
 if ( ! defined( 'CIVICRM_WP_EVENT_ORGANISER_FILE' ) ) {
