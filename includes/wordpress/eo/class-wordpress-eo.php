bba42b600b7d79855062eb686c2a8441b98a256e
Christian Wach <needle@haystack.co.uk>
2025-09-01 13:13:42 +0100

    Remove debugging code

--- includes/wordpress/eo/class-wordpress-eo.php
+++ includes/wordpress/eo/class-wordpress-eo.php
@@ -1089,14 +1089,6 @@ class CEO_WordPress_EO {
 		// Get linked CiviCRM Events.
 		$civi_events = $this->plugin->mapping->get_civi_event_ids_by_eo_event_id( $event->ID );
 
-		$e = new \Exception();
-		$trace = $e->getTraceAsString();
-		error_log( print_r( [
-			'method' => __METHOD__,
-			'civi_events' => $civi_events,
-			//'backtrace' => $trace,
-		], true ) );
-
 		// Set "multiple linked" status.
 		$multiple_linked = false;
 		if ( count( $civi_events ) > 1 ) {
